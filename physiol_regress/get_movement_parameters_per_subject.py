7f3b7fda282d5fe0489ada0ee543c4588b4bfbdf
uqkgarn1 <uqkgarn1@gpunode-2-1.localdomain>
2022-01-19 20:17:20 +1000

    developig extracting motion regressors

--- physiol_regress/get_movement_parameters_per_subject.py
+++ physiol_regress/get_movement_parameters_per_subject.py
@@ -1,3 +1,25 @@
-# 
+# get motion regressors into a .txt file for use in PhysIO toolbox
+### written by K. Garner, 2022
+
+# %%[intro]
+#
+# all python related activities for this pipeline are done in the environment certval_pline
 
 # %%
+import pandas as pd
+import numpy as np
+import os, re, json
+
+# %%[] 
+# First define parameters and collect subject/session iterables
+
+# %%
+data_dir = '/clusterdata/uqkgarn1/scratch/data/'
+subject_number = '01' 
+session_number = pd.Series(str(2)) # this assumes data is in BIDS
+runs = pd.Series(str(x) for x in [1, 2, 3])
+
+# %%[]
+# Now making a list of the confounds file for that participant
+
+# %% s
