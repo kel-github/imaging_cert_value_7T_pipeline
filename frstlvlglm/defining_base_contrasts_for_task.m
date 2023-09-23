3e7bdac9df84124f0e9cfbde42e2581c4ec8e573
Neurodesk User <user@neurodesk.github.io>
2023-09-23 10:58:47 +0000

    task contrasts defined

--- frstlvlglm/defining_base_contrasts_for_task.m
+++ frstlvlglm/defining_base_contrasts_for_task.m
@@ -15,27 +15,44 @@
 %     {'left_.5_hh'}    {'left_.5_hl'}    {'left_.5_lh'}    {'left_.5_ll'}    {'left_.8_hh'}    {'left_.8_hl'}    {'left_.8_lh'}    {'left_.8_ll'}    {'right_.5_hh'}
 %     {'right_.5_hl'}    {'right_.5_lh'}    {'right_.5_ll'}    {'right_.8_hh'}    {'right_.8_hl'}    {'right_.8_lh'}    {'right_.8_ll'} }
 
-% main effect of certainty
-% cond %tgtloc %cert %tgtlocxcert
-% l5       1     1      1
-% l8       1    -1     -1
-% r5      -1     1     -1
-% r8      -1    -1      1
-k_mat = [1 1 1 1];
-certbyloc = [1 -1 -1 1];
-certbyloc = kron(certbyloc, k_mat);
+% 16 task regressors x 3 HRF parameters = 48 total
 
-% main effect of left vs right value
-% cond v1  v2  v3
-% hh    1   0  0
-% hl   -1   1  0
-% lh    0  -1  1
-% ll    0   0 -1
-me_val_conf = [1 -1  0  0;...
-               0  1 -1  0;...
-               0  0  1 -1];
-me_val_conf = repmat(me_val_conf, 1, length(k_mat));
-me_val_conf = me_val_conf / length(k_mat);
+% main effect of certainty left > right
+% cond %tgtloc %cert %tgtlocxcert
+% l5       1    -1     -1
+% l8       1     1      1
+% r5      -1    -1      1
+% r8      -1     1     -1
+
+cert_left = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+cert_right = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+cert_left = [cert_left, zeros([1, length(cert_left)])];
+cert_right = [zeros(1, length(cert_right)), cert_right];
+
+certbyloc = [-1 0 0 1 0 0 1 0 0 -1 0 0];
+certbyloc = [repmat(certbyloc(1:3),1,4), ...
+             repmat(certbyloc(4:6),1,4), ...
+             repmat(certbyloc(7:9),1,4), ...
+             repmat(certbyloc(10:12),1,4)];
+
+
+% effect of left != right value vs left value == right value
+% cond 
+% a positive value for the first suggests that the
+% voxel's activity is increased when high value is on the 
+% left
+% a positive value for the 2nd suggests that its actity is
+% increased when high value is on the right.
+% So, a voxel that cares about value in the left hemifield
+% should be +/-
+% hh   -0.5   -0.5        
+% hl      1      0
+% lh      0      1
+% ll   -0.5   -0.5   
+rel_val_left = [-0.5  0 0  1 0 0   0 0 0  -0.5 0 0];
+rel_val_left = repmat(rel_val_left, 1, 4);
+rel_val_right = [-0.5  0 0  0 0 0  1 0 0  -0.5 0 0];
+rel_val_right = repmat(rel_val_right, 1, 4);
 
 % counterfactual; no value conflict
 % cond  tgt  cert  val tgt valxtgt
@@ -55,7 +72,8 @@ me_val_conf = me_val_conf / length(k_mat);
 % hl     r    .8    0  0      0
 % lh     r    .8    0  0      0
 % ll     r    .8   -1 -1      1 
-cf_noconf = [1 0 0 -1 1 0 0 -1 -1 0 0 1 -1 0 0 1]/4;
+cf_noconf = [1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 ...
+             0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0];
 
 % counterfactual; value conflict
 % cond  tgt  cert  val tgt valxtgt
@@ -75,7 +93,8 @@ cf_noconf = [1 0 0 -1 1 0 0 -1 -1 0 0 1 -1 0 0 1]/4;
 % hl     r    .8    1 -1     -1
 % lh     r    .8   -1 -1      1
 % ll     r    .8    0  0      0 
-cf_conf = [0 1 -1 0 0 1 -1 0 0 -1 1 0 0 - 1 1 0]/4;
+cf_conf = [0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 -1 0 0 ...
+           1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0];
 
-save('task_contrasts', 'certbyloc',...
-    'me_val_conf', 'cf_noconf', 'cf_conf');
+save('task_contrasts', 'cert_left', 'cert_right', 'certbyloc',...
+    'rel_val_left', 'rel_val_right', 'cf_noconf', 'cf_conf');
--- frstlvlglm/run_task_contrasts_over_subs.m
+++ frstlvlglm/run_task_contrasts_over_subs.m
@@ -1,71 +1,90 @@
 %-----------------------------------------------------------------------
-% Job saved on 22-Sep-2023 09:59:05 by cfg_util (rev $Rev: 7345 $)
+% Job saved on 23-Sep-2023 10:36:08 by cfg_util (rev $Rev: 7345 $)
 % spm SPM - Unknown
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-% run this script to define all contrasts for the task comparisons, over
-% all subjects
-% K. Garner, 2023
+% K. Garner  2023
+% defining 1st level contrasts to get effects of spatial certainty and value
+% this script will loop over subjects and
+% 1) try catch for the sub's spm file
+% 2) compute contrasts for
+%     -- effects of interest (F)
+%     -- cert x tgt loc int (T)
+%     -- effect of value left > value right (compared to average of ll &
+%     hh) (T)
+%     -- effect of value left < value right (compared to average of ll &
+%     hh) (T)
+%     -- countrefactual model under conditions of no regret (T)
+%     -- counterfactual model under conditions of regret
+%     -- increasing spatial certainty to the left (p.5 < p.8) (T)
+%     -- as above, but for right (T)
+%
+% to run this code in the batch command on neurodesk, go to neurodesk >
+% functional imaging > spm (not gui)
+% enter below at command line
 % run_spm12.sh /opt/mcr/v97/ batch /home/jovyan/neurodesktop-storage/imaging_cert_value_7T_pipeline/frstlvlglm/run_task_contrasts_over_subs.m
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Define subject parameters and file locations
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-addpath('/home/jovyan/neurodesktop-storage/imaging_cert_value_7T_pipeline/frstlvlglm/'); % for contrast building functions
 
-% this is where all the SPM mat files live (in BIDS format
-% sub-x/ses-0x/func/)
-rootfolder = '/data/VALCERT/derivatives/fl_glm/';
+subfol = {'001', '004','006','008','017','020','024','025','075','076','078','079','080','124','126','128','129','130','132','133','134','135','152','151'};
+% define where path for the glms
+flglm_dir = '/data/VALCERT/derivatives/fl_glm/task/sub-%s/SPM/SPM.mat';
 
-%subs = {'01','04','06','08','17','20','24','25','75','76','78','79','80', '84','124','126','128','129','130','132','133','134','135','152','151'};
-%subfol00 = {'001','004','006','008','017','020','024','025','075','076','078','079','080','084','124','126','128','129','130','132','133','134','135','152','151'};
-% test run
-subs = {'01'};
-subfol00 = {'001'};
+for isub = 1:length(subfol)
 
-% load base contrasts
-load('/home/jovyan/neurodesktop-storage/imaging_cert_value_7T_pipeline/frstlvlglm/task_contrasts.mat')
+    if (exist(sprintf(flglm_dir, subfol{isub}), 'file'))
 
-for isub = 1:length(subs)
-    
-    clear matlabbatch
-    this_sub = subs{isub}; % get this subject number        
-    fprintf("current subject: %s", this_sub)
-    fprintf("\n")
+        fprintf(sprintf('estimating contrasts for sub %s', subfol{isub}));
 
-    sub_fol = subfol00{isub};
+        matlabbatch{1}.spm.stats.con.spmmat = {sprintf(flglm_dir, subfol{isub})};
+        matlabbatch{1}.spm.stats.con.consess{1}.fcon.name = 'effects-of-interest';
+        %%
+        matlabbatch{1}.spm.stats.con.consess{1}.fcon.weights = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0];
+        %%
+        matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'cert-by-loc';
+        matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'rel-val-left';
+        matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [-0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0];
+        matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'rel-val-right';
+        matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [-0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0];
+        matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'counterfact-no-regret';
+        matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'counterfact-regret';
+        matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0];
+        matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'cert_left';
+        matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+        matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'cert-right';
+        matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.delete = 1;
 
-    % first get the n of nuisance regressors per run for this sub
-    n_nuisance = contrast_zero_count(rootfolder, 'SPM.mat', sub_fol, 'task');
 
-    matlabbatch{1}.spm.stats.con.spmmat = {'/data/VALCERT/derivatives/fl_glm/task/sub-001/SPM/SPM.mat'};
+        spm('defaults', 'FMRI');
+        spm_jobman('initcfg');
+        spm_jobman('run',matlabbatch);
 
-    matlabbatch{1}.spm.stats.con.consess{1}.fcon.name = 'certbyloc';
-    % use base contrast certbyloc to generate sub contrast
-    sub_certbyloc = contrast_matrix_builder(certbyloc, n_nuisance);
-
-    % add it to the info
-    matlabbatch{1}.spm.stats.con.consess{1}.fcon.weights = sub_certbyloc;
-    matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'replsc';
-
-    matlabbatch{1}.spm.stats.con.consess{2}.fcon.name = 'me_val_conf';
-    sub_me_val_conf = contrast_matrix_builder(me_val_conf, n_nuisance);
-
-    matlabbatch{1}.spm.stats.con.consess{2}.fcon.weights = sub_me_val_conf;
-    matlabbatch{1}.spm.stats.con.consess{2}.fcon.sessrep = 'replsc';
-
-    matlabbatch{1}.spm.stats.con.consess{3}.fcon.name = 'cf_noconf';
-    sub_cf_noconf = contrast_matrix_builder(cf_noconf, n_nuisance);
-    matlabbatch{1}.spm.stats.con.consess{3}.fcon.weights = sub_cf_noconf;
-    matlabbatch{1}.spm.stats.con.consess{3}.fcon.sessrep = 'replsc';
-
-    matlabbatch{1}.spm.stats.con.consess{4}.fcon.name = 'cf_conf';
-    sub_cf_conf = contrast_matrix_builder(cf_conf, n_nuisance);
-    matlabbatch{1}.spm.stats.con.consess{4}.fcon.weights = sub_cf_conf;
-    matlabbatch{1}.spm.stats.con.consess{4}.fcon.sessrep = 'replsc';
-
-    matlabbatch{1}.spm.stats.con.delete = 0;
-
-    spm('defaults', 'FMRI');
-    spm_jobman('initcfg');
-    spm_jobman('run',matlabbatch);
+    else
+        fprintf(sprintf('no valid spm mat file found for sub %s, check fl glm'),...
+            subfol{isub});
+    end
 end
