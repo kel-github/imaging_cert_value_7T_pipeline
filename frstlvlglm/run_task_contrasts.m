4a298faf18dcc7fb30eebe29f3d09e3776b1329a
Neurodesk User <user@neurodesk.github.io>
2023-10-04 07:29:47 +0000

    troubleshooting main effect of session/task

---
+++ frstlvlglm/run_task_contrasts.m
@@ -0,0 +1,48 @@
+%-----------------------------------------------------------------------
+% Job saved on 23-Sep-2023 10:29:19 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - Unknown
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.con.spmmat = {'/data/VALCERT/derivatives/fl_glm/task/sub-001/SPM/SPM.mat'};
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.name = 'effects-of-interest';
+%%
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.weights = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+                                                        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0];
+%%
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'cert-by-loc';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'rel-val-left';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [-0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0];
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'rel-val-right';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [-0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0];
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'counterfact-no-regret';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0];
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'counterfact-regret';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'cert_left';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'cert-right';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'replsc';
+matlabbatch{1}.spm.stats.con.delete = 1;
--- frstlvlglm/run_task_contrasts_over_subs.m
+++ frstlvlglm/run_task_contrasts_over_subs.m
@@ -24,14 +24,14 @@
 % enter below at command line
 % run_spm12.sh /opt/mcr/v97/ batch /home/jovyan/neurodesktop-storage/imaging_cert_value_7T_pipeline/frstlvlglm/run_task_contrasts_over_subs.m
 
-
-subfol = {'001', '004','006','008','017','020','024','025','075','076','078','079','080','124','126','128','129','130','132','133','134','135','152','151'};
-subfol = {'076'};%{'004','006','008','017','020','024','025','075','076','078','079','080','084','124','126','128','129','130','132','133','134','135','137','152','151'};%{'001','002'}
+%subfol = {'001', '004','006','008','017','020','024','025','075','076','078','079','080','124','126','128','129','130','132','133','134','135','152','151'};
+subfol = {'001','004','006','008','017','020','024','025','075','076','078','079','080','084','124','126','128','129','130','132','133','134','135','137','152','151'};%{'001','002'}
 
 % define where path for the glms
 flglm_dir = '/data/VALCERT/derivatives/fl_glm/task/sub-%s/SPM/SPM.mat';
 
 for isub = 1:length(subfol)
+    clear matlabbatch
 
     if (exist(sprintf(flglm_dir, subfol{isub}), 'file'))
 
@@ -58,27 +58,33 @@ for isub = 1:length(subfol)
             0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0];
         %%
         matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'cert-by-loc';
-        matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0];
+        
+        %%% create separate tcons for second level effects of interest
+
+        matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'fxoftask';
+        matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
         matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'rel-val-left';
-        matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [-0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0];
+        matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'cert-by-loc';
+        matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0];
         matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'rel-val-right';
-        matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [-0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0];
+        matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'rel-val-left';
+        matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [-0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0 -0.5 0 0 1 0 0 0 0 0 -0.5 0 0];
         matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'counterfact-no-regret';
-        matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'rel-val-right';
+        matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [-0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0 -0.5 0 0 0 0 0 1 0 0 -0.5 0 0];
         matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'counterfact-regret';
-        matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0];
+        matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'counterfact-no-regret';
+        matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0];
         matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'cert_left';
-        matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+        matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'counterfact-regret';
+        matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 0 0 0 -1 0 0 1 0 0 0 0 0];
         matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'replsc';
-        matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'cert-right';
-        matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'cert_left';
+        matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [-1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
         matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'replsc';
+        matlabbatch{1}.spm.stats.con.consess{9}.tcon.name = 'cert-right';
+        matlabbatch{1}.spm.stats.con.consess{9}.tcon.weights = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 -1 0 0 -1 0 0 -1 0 0 1 0 0 1 0 0 1 0 0 1 0 0];
+        matlabbatch{1}.spm.stats.con.consess{9}.tcon.sessrep = 'replsc';
         matlabbatch{1}.spm.stats.con.delete = 1;
 
 
